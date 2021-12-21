63d07476b8991f22e01542f0ae6a4d330da33a99
Обеднин Кирилл <kirillobednin1@gmail.com>
2021-12-21 13:20:56 +0800

    Добавьте файлы проекта.

---
+++ CourseWork/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CourseWork
+{
+    public partial class Form1 : Form
+    {
+        List<Particle> particles = new List<Particle>();//пустой список частиц
+        public Form1()
+        {
+            InitializeComponent();
+            picDisplay.Image = new Bitmap(picDisplay.Width, picDisplay.Height);// привязка изображения
+
+            // генерирация 500 частиц
+            for (var i = 0; i < 500; ++i)
+            {
+                var particle = new Particle();
+                // переношу частицы в центр изображения
+                particle.X = picDisplay.Image.Width / 2;
+                particle.Y = picDisplay.Image.Height / 2;
+                // добавляю список
+                particles.Add(particle);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            using (var g = Graphics.FromImage(picDisplay.Image))
+            {
+                g.Clear(Color.White); // очистка picDisplay
+            }
+
+            picDisplay.Invalidate();//обновление picDisplay
+        }
+    }
+}
---
+++ CourseWork/Particle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseWork
+{
+    class Particle
+    {
+        public int Radius; // радуис частицы
+        public float X; // X координата положения частицы в пространстве
+        public float Y; // Y координата положения частицы в пространстве
+
+        public float Direction; // направление движения
+        public float Speed; // скорость перемещения
+
+        public static Random rnd = new Random(); //генератор случайных чисел
+
+        // конструктор по умолчанию будет создавать кастомную частицу
+        public Particle()
+        {
+            // все частицы будут возникать из одного места
+            Direction = rnd.Next(360);
+            Speed = 1 + rnd.Next(10);
+            Radius = 2 + rnd.Next(10);
+        }
+    }
+}
