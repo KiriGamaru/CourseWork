bc6d72c078c724a57f9850f189d01e9704dacd5e
Обеднин Кирилл <kirillobednin1@gmail.com>
2021-12-21 14:15:36 +0800

    Создал цветные частицы с векторной скоростью, которые вылетают из места положения курсора.

--- CourseWork/Form1.cs
+++ CourseWork/Form1.cs
@@ -17,27 +17,92 @@ namespace CourseWork
         {
             InitializeComponent();
             picDisplay.Image = new Bitmap(picDisplay.Width, picDisplay.Height);// привязка изображения
+        }
 
-            // генерирация 500 частиц
-            for (var i = 0; i < 500; ++i)
+        // добавил функцию обновления состояния системы
+        private void UpdateState()
+        {
+            foreach (var particle in particles)
             {
-                var particle = new Particle();
-                // переношу частицы в центр изображения
-                particle.X = picDisplay.Image.Width / 2;
-                particle.Y = picDisplay.Image.Height / 2;
-                // добавляю список
-                particles.Add(particle);
+                particle.Life -= 1;// уменьшаю здоровье
+                                    
+                if (particle.Life < 0)// если здоровье кончилось
+                {
+                    particle.Life = 20 + Particle.rnd.Next(100);// восстанавливаю здоровье
+
+                    // перемещаю частицу в место положения курсора
+                    particle.X = MousePositionX;
+                    particle.Y = MousePositionY;
+
+                    //сброс состояния частицы
+                    var direction = (double)Particle.rnd.Next(360);
+                    var speed = 1 + Particle.rnd.Next(10);
+
+                    particle.SpeedX = (float)(Math.Cos(direction / 180 * Math.PI) * speed);
+                    particle.SpeedY = -(float)(Math.Sin(direction / 180 * Math.PI) * speed);
+
+                    particle.Radius = 2 + Particle.rnd.Next(10);// рандомный размер частицы
+                }
+                else {
+                    particle.X += particle.SpeedX;
+                    particle.Y += particle.SpeedY;
+                }
+            }
+
+            // генерация частиц
+            // генерирую не более 10 штук за тик
+            for (var i = 0; i < 10; ++i)
+            {
+                if (particles.Count < 500) // пока частиц меньше 500 генерируем новые
+                {
+                    var particle = new ParticleColorful();
+                    particle.FromColor = Color.Yellow;
+                    particle.ToColor = Color.FromArgb(0, Color.Magenta);
+                    particle.X = MousePositionX;
+                    particle.Y = MousePositionY;
+                    particles.Add(particle);
+                }
+                else
+                {
+                    break; // а если частиц уже 500 штук, то ничего не генерирую
+                }
+            }
+
+        }
+
+        // функция рендеринга
+        private void Render(Graphics g)
+        {
+            // утащили сюда отрисовку частиц
+            foreach (var particle in particles)
+            {
+                particle.Draw(g);
             }
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
+            UpdateState(); // каждый тик обновляем систему
+
             using (var g = Graphics.FromImage(picDisplay.Image))
             {
-                g.Clear(Color.White); // очистка picDisplay
+                g.Clear(Color.Black); // очистка picDisplay
+                Render(g); // рендерим систему
             }
 
             picDisplay.Invalidate();//обновление picDisplay
+        }
+
+
+        // добавляем переменные для хранения положения мыши
+        private int MousePositionX = 0;
+        private int MousePositionY = 0;
+
+        private void picDisplay_MouseMove(object sender, MouseEventArgs e)
+        {
+            //заносим положение мыши в переменные для хранения положения мыши
+            MousePositionX = e.X;
+            MousePositionY = e.Y;
         }
     }
 }
